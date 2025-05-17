c88e2135aa3e957d2a650d183c20af75bea1539b
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2025-05-17 15:24:39 +0300

    task3

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { ErrorHandler } from './middlewares/ErrorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import productsRouter from './routers/products.js';
 
 import { env } from './utils/env.js';
 
@@ -13,11 +14,11 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  //   app.use(productsRouter);
+  app.use(productsRouter);
 
-  //   app.use('*', notFoundHandler);
+  app.use('*', notFoundHandler);
 
-  //   app.use(errorHandler);
+  app.use(ErrorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
