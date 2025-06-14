be1d2c2223a75c4bdfbf72fd0b4dc5dc5d367279
Larysa <seagull2610@gmail.com>
2025-06-14 15:36:00 +0200

    tasks 5-6

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 
 import { env } from './utils/env.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -16,6 +17,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
